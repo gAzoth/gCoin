1586c8027a1ba4d2e073c5b19210e47a13fd5bc9
gAzoth <johnhgaffney@gmail.com>
2025-03-14 17:33:22 -0400

    changed to gCoin

--- main.go
+++ main.go
@@ -5,8 +5,8 @@ import "fmt"
 func main() {
 	bc := NewBlockchain()
 
-	bc.AddBlock("Send 1 BTC to Ivan")
-	bc.AddBlock("Send 2 more BTC to Ivan")
+	bc.AddBlock("Send 1 gCoin to Ivan")
+	bc.AddBlock("Send 2 more gCoin to Ivan")
 
 	for _, block := range bc.blocks {
 		fmt.Printf("Prev. hash: %x\n", block.PrevBlockHash)
